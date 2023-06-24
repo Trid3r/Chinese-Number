b11b06516a99ded28bd55310bdb2b075f8fdb1f9
Trid3r <alex_yhd@hotmail.com>
2023-06-24 02:02:44 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.use(Vuetify);
+const vuetify = new Vuetify();
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App)
+}).$mount("#app");
